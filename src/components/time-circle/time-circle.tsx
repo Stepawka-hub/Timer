cbe5f1171df36a8d0a285cbd89ca104aa62ffe69
Stepawka <stepan-lvov-04@mail.ru>
2025-05-09 05:12:14 +0300

    feat: Исправление лишних волн при смене вкладок

--- src/components/time-circle/time-circle.tsx
+++ src/components/time-circle/time-circle.tsx
@@ -1,10 +1,29 @@
-import { FC, memo } from "react";
+import { FC, memo, useEffect, useRef } from "react";
 import s from "./time-circle.module.css";
 import { TimeCircleProps } from "./type";
 
 export const TimeCircle: FC<TimeCircleProps> = memo(({ onClick, children }) => {
+  const circleRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleVisibilityChange = () => {
+      if (document.hidden && circleRef.current) {
+        circleRef.current.blur();
+      }
+    };
+
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+    return () =>
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+  }, []);
+
   return (
-    <div className={s.timeCircle} onClick={onClick} tabIndex={0}>
+    <div
+      ref={circleRef}
+      className={s.timeCircle}
+      onClick={onClick}
+      tabIndex={0}
+    >
       {children}
     </div>
   );
